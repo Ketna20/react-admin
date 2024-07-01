2ed96b0e6f6efab3f57a094f563f2bd921358de6
ketna20 <ketna.kumar@gmail.com>
2024-07-01 08:10:37 -0700

    Topbar implementation in progress

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+Installed these for this project: 
+$ npm i @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar formik yup @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo
+
+
+## Available Script
 
 In the project directory, you can run:
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseLine, ThemeProvider } from  '@mui/material';
+import Topbar from './scenes/global/Topbar';
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <div className="app">
+          <main className='content'>
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
